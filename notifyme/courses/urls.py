d8fff08c48bbc467a980dbe87ede392c36714bf9
Vikas <vikas_panwar@iitb.ac.in>
2020-12-06 02:34:14 +0530

    added course list

--- notifyme/courses/urls.py
+++ notifyme/courses/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_course, name="courses"),
+    path('list', views.list_courses, name="list"),
+    path('', views.add_course, name="courses")
 ]
--- notifyme/courses/views.py
+++ notifyme/courses/views.py
@@ -9,5 +9,20 @@ def add_course(request):
         secret_key = request.POST['sk']
         course = Course(instructor=instructor, course_name=course_name, course_id=course_id, secret_key=secret_key)
         course.save()
+        return redirect('/courses/list')
+        print("vikas*******************")
     else:
+        print("m*******************")
         return render(request, 'courses.html')
+
+def list_courses(request):
+    courses=Course.objects.filter(instructor=request.user.username)
+    #print(request.user.username)
+    # unseen = []
+    # for message in messages:
+    #     if message.user not in message.seen.all():
+    #         message.seen.add(message.user)
+    #     if request.user not in message.seen.all():
+    #         unseen.append(message)
+
+    return render (request,'list_courses.html',{"obj_list": courses})
