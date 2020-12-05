9401f046e229a12398d4a8d624cd0d3c4ac02053
Vikas <vikas_panwar@iitb.ac.in>
2020-12-05 22:45:01 +0530

    init

--- notifyme/notifyme/urls.py
+++ notifyme/notifyme/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('', include('Hirenweb.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('notify/', include('notify.urls'))
+    path('notify/', include('notify.urls')),
+    path('student', views.student, name = "profile")
 ]
