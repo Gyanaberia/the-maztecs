3468473d5166476942d85f927e09b0c0a0faa20d
Vikas <vikas_panwar@iitb.ac.in>
2020-12-08 13:50:38 +0530

    student

--- notifyme/student/views.py
+++ notifyme/student/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 def register(request):
-    data=request.POST
-    print(data)
+    if request.method=='POST':
+        data=request.POST
+        print(data)
+        username=data[0]
+        email=data[1]
+        password=data[2]
+        stu=Student(username=username, email=email, password=password, course_id=None)
+        stu.save()
